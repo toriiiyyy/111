efa0434626f4da01c9c5586ab2a6402ce5269879
toriiiyyy <asgerto9@gmail.com>
2026-07-06 16:14:17 +0300

    Delete src directory

--- src/features/chams/chams.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdint.h>
-namespace chams
-{
-	using OnGeneratePrimitivesFn = __int64(__fastcall*)(__int64 desc, __int64 object, __int64 a3, __int64 render_buf);
-	inline OnGeneratePrimitivesFn oOnGeneratePrimitives = nullptr;
-
-	__int64 hkOnGeneratePrimitives(__int64 desc, __int64 object, __int64 a3, __int64 render_buf);
-}
